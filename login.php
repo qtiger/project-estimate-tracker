e83e7b8aaa4aea5944357562c542bc39ff3cb62b
ian <ian@quantumtiger.org>
2011-10-11 07:48:42 +0000

    Initial Load

---
+++ login.php
@@ -0,0 +1,61 @@
+<?php
+if(!isset($_SESSION))
+  session_start();
+
+require('init_smarty.php');
+require ('tmUtils.php');
+DBConnect();
+
+class gFormVar
+  {
+  public $error="";
+  public $stylesheet='normal.css';
+  public $pageTitle='Project Tracking Database';
+  }
+$g = new gFormVar(); 
+
+if (!$sqlSess)
+  {
+  echo "connect failure";
+  $g->error = "not_connected";
+  }
+
+$loggedIn = FALSE;
+
+if (isset($_POST['Action']))
+  {
+  if (isset($_POST['UserName']))
+    {
+    if ($_POST['UserName'] == "")
+      { $g->error="name_empty"; }
+    else
+      {
+      $user = GetUser($_POST['UserName']);
+      if ($user[1] != "")
+        {
+        $expire = time()+60*60*24*7;
+        
+        setcookie("username",$_POST['UserName'],$expire);
+        setcookie("userfullname",$user[1], $expire);
+        setcookie("userno",$user[2], $expire);
+        
+        $loggedIn=TRUE;
+        }
+      else
+        { $g->error="name_wrong"; }
+      }
+    }
+  else
+    { $g->error="name_empty"; }
+  }
+
+if ($loggedIn)
+  {
+  header( 'Location: matrix.php' );
+  }
+else
+  {
+  $tmpl->assign('g',$g);
+  $tmpl->display('login.tpl');
+  }
+?>
