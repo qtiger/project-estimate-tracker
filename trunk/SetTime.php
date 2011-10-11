cb6ef08a37bc4f8921889f4244f11f92f3903c22
ian@quantumtiger.org <ian@quantumtiger.org@033006fc-0a1e-2fcd-f6aa-1b5260ef9ce4>
2011-10-11 07:52:35 +0000

    Initial Load

---
+++ trunk/SetTime.php
@@ -0,0 +1,95 @@
+<?php
+class gFormVar
+  {
+  public $devID;
+  public $date;
+}
+
+require ('tmUtils.php');
+require('init_smarty.php');
+DBConnect();
+
+if ($_SERVER['REQUEST_METHOD']=='POST')
+  {
+  parse_str($_SERVER['QUERY_STRING'],$query);
+  
+  $allOk="";
+  if (!array_key_exists("action", $query)) $allOk .= "action not set.";
+  if (!array_key_exists("user", $query)) $allOk .= "user not set. ";
+  if (!array_key_exists("date", $query)) $allOk .= "date not set. ";
+
+  if ($allOk=="")
+    {
+    $g = new gFormVar(); 
+    
+    $g->devID = $query['user'];
+    $g->date = $query['date'];
+
+    if ($query['action'] == 'add')
+      {
+      if (!array_key_exists("task", $query)) $allOk .= "task not set. ";
+      if (!array_key_exists("minutes", $query)) $allOk .= "minutes not set. ";
+
+      if (array_key_exists("sub", $query)) $sub = $query['sub'];
+      else $sub = '';
+
+      if ($allOk=="")
+        {
+        $sql = "insert into time (taskid,developerid,minutes,date,starttime,description) values (" .
+        $query['task'] . "," . $query['user'] . "," . $query['minutes'] .
+        ",'". $query['date'] . "',now(), '$sub')";
+        
+        $res = mysql_query($sql, $sqlSess);
+        }
+      }
+    else if ($query['action'] == 'delete')
+      {
+      if (!array_key_exists("timeid", $query)) $allOk .= "timeif not set. ";
+      
+      if ($allOk=="")
+        {
+        $res = mysql_query("delete from time where timeid = " . $query['timeid'], $sqlSess);
+        }
+      }
+    else if ($query['action'] == 'update')
+      {
+      if (!array_key_exists("timeid", $query)) $allOk .= "timeif not set. ";
+      if (!array_key_exists("minutes", $query)) $allOk .= "minutes not set. ";
+      
+      if ($allOk=="")
+        {
+        if (array_key_exists("sub", $query)) $sub = $query['sub'];
+        else $sub = '';
+
+        if ($query['minutes'] ==0)
+          {
+          $res = mysql_query("select timediff(now(), starttime) from time where timeid = " . $query['timeid'], $sqlSess);
+          
+          if ($res)
+            {
+            $timeArr = mysql_fetch_array($res);
+            $time = explode(":",$timeArr[0]);
+            
+            $mins = ($time[0] * 60) + $time[1];           
+            if ($time[2] >=30) $mins++; 
+            
+            $res = mysql_query("update time set minutes = $mins, description = '$sub' where timeid = " . $query['timeid'], $sqlSess);
+            }
+          }
+        else
+          $res = mysql_query("update time set minutes = " . $query['minutes'] . ", description = '$sub' where timeid = " . $query['timeid'], $sqlSess);
+        }
+      }
+    if ($allOk=="")
+      {
+      include "timesheetsub.php";
+
+      $tmpl->assign('g',$g);
+      echo $tmpl->fetch('timesheetsub.tpl');
+      }
+    else echo "Incorrect Params(2): " . $allOk;
+    }
+  else echo "Incorrect Params(3): " . $allOk;
+  }
+else echo "Incorrect Method. Expecting Post.";
+?>
