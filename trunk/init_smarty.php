567374ca4b96683a8eea7a39c4f74623a267e94d
ian@quantumtiger.org <ian@quantumtiger.org@033006fc-0a1e-2fcd-f6aa-1b5260ef9ce4>
2011-10-11 12:13:28 +0000

    Check Smarty path is correct before run

--- trunk/init_smarty.php
+++ trunk/init_smarty.php
@@ -1,19 +1,26 @@
 <?php
 // put full path to Smarty.class.php
-require('/Smarty/Smarty.class.php');
-$tmpl = new Smarty();
+$smartyPath = "../../Smarty/Smarty.class.php";
+$templatesOK = false;
+if (file_exists($smartyPath))
+  {
+  @require($smartyPath);
+  $templatesOK = true;
 
-$tmpl->template_dir = 'taskmon_templates/templates';
-$tmpl->compile_dir = 'taskmon_templates/templates_c';
-$tmpl->cache_dir = 'taskmon_templates/cache';
-$tmpl->config_dir = 'taskmon_templates/configs';
+  $tmpl = new Smarty();
 
-// Session specifics
-session_start();
+  $tmpl->template_dir = 'taskmon_templates/templates';
+  $tmpl->compile_dir = 'taskmon_templates/templates_c';
+  $tmpl->cache_dir = 'taskmon_templates/cache';
+  $tmpl->config_dir = 'taskmon_templates/configs';
 
-if (isset($_SESSION['devID'])) { $devID = $_SESSION['devID']; }
-else $devID = "";
-if (isset($_SESSION['projID'])) { $projID = $_SESSION['projID']; }
-else $projID = "";
+  // Session specifics
+  session_start();
 
+  if (isset($_SESSION['devID'])) { $devID = $_SESSION['devID']; }
+  else $devID = "";
+  if (isset($_SESSION['projID'])) { $projID = $_SESSION['projID']; }
+  else $projID = "";
+  }
+else echo "<h1>PET Templates not found</h1><p>Could not find Smarty Template Classes. Please check smarty Path in init_smarty.php</p>"
 ?>
