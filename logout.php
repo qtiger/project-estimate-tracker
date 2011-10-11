ec3097e4a4ae176c1abc6e5aee91c4909b1319fc
ian <ian@quantumtiger.org>
2011-10-11 07:49:04 +0000

    Initial Load

---
+++ logout.php
@@ -0,0 +1,28 @@
+<?php
+setcookie("username","", -3600);
+setcookie("userfullname","", -3600);
+setcookie("userno","", -3600);
+?>
+
+<html>
+<head>
+<link rel='StyleSheet' href='normal.css' type='text/css' title='NormalStyle' media='screen, print'>
+<title>Project Tracking Database</title>
+</head>
+
+<body>
+<div id='main'>
+<hr>
+<h1>Project Tracking Database</h1>
+<hr><h2>Logout</h2>
+<hr>
+
+<p>Thanks for using the project tracking database. Come back soon!</p>
+
+<button onClick="window.location='login.php'">Log back in</button> &nbsp;
+<hr>
+&copy; 2008 Tearfund<br>
+All rights reserved
+<hr></div>
+</body>
+</html>
