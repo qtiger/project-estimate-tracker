a55be5f9b3d667381becfa684cced5bd673f267e
ian@quantumtiger.org <ian@quantumtiger.org@033006fc-0a1e-2fcd-f6aa-1b5260ef9ce4>
2011-10-11 08:00:55 +0000

    Initial Load

---
+++ trunk/tsrep_sub.php
@@ -0,0 +1,21 @@
+<?php
+$g->monthStart = date($g->year . "-" . $g->mon ."-01");
+$monthEnd = strtotime ( '+1 month' , strtotime ( $g->monthStart ) ) ;
+$g->monthEnd = date ("Y-m-d",$monthEnd);
+
+$g->daysInMonth = cal_days_in_month  (CAL_GREGORIAN,$g->mon,$g->year);
+
+$ts = TaskArray($g->monthStart, $g->monthEnd, $g->devID);
+
+$g->dayOfWeek = date("w",strtotime($g->monthStart));
+if ($g->dayOfWeek == 0) $g->dayOfWeek = 7;
+
+if (is_array($ts)) $g->error = "";
+else $g->error = $ts;
+
+$g->ts = $ts;
+
+$g->totals = TotalsArray($g->monthStart, $g->monthEnd, $g->devID);
+
+$tmpl->register_function("minsToHours","minsToHours");
+?>
