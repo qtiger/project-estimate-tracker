8406c5f49a5e208625a2d7c6352cf037708b3bf0
ian <ian@quantumtiger.org>
2011-10-11 14:18:09 +0000

    Obsolete

--- dl.php
+++ dl.php
@@ -1,4 +1,7 @@
 <?php
+/////////////////////////////////////////////////////////////////
+// Obsolete
+////////////////////////////////////////////////////////////////  
 header("Content-type: application/octet-stream");
 header("Content-Disposition: attachment; filename=\"my-data.csv\"");
 $data="col1, col start and end col2,col3, \n";
