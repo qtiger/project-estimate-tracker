55fe6695e7b986171a4af184b822b996d1fcb70c
thequantumtiger <thequantumtiger@033006fc-0a1e-2fcd-f6aa-1b5260ef9ce4>
2011-10-12 19:02:17 +0000

    Error check to ensure Smarty present before proceding

--- trunk/login.php
+++ trunk/login.php
@@ -3,59 +3,61 @@ if(!isset($_SESSION))
   session_start();
 
 require('init_smarty.php');
-require ('tmUtils.php');
-DBConnect();
-
-class gFormVar
+if ($templatesOK)
   {
-  public $error="";
-  public $stylesheet='normal.css';
-  public $pageTitle='Project Tracking Database';
-  }
-$g = new gFormVar(); 
+  require ('tmUtils.php');
+  DBConnect();
 
-if (!$sqlSess)
-  {
-  echo "connect failure";
-  $g->error = "not_connected";
-  }
-
-$loggedIn = FALSE;
-
-if (isset($_POST['Action']))
-  {
-  if (isset($_POST['UserName']))
+  class gFormVar
     {
-    if ($_POST['UserName'] == "")
-      { $g->error="name_empty"; }
-    else
+    public $error="";
+    public $stylesheet='normal.css';
+    public $pageTitle='Project Tracking Database';
+    }
+  $g = new gFormVar(); 
+
+  if (!$sqlSess)
+    {
+    $g->error = "not_connected";
+    }
+
+  $loggedIn = FALSE;
+
+  if (isset($_POST['Action']))
+    {
+    if (isset($_POST['UserName']))
       {
-      $user = GetUser($_POST['UserName']);
-      if ($user[1] != "")
-        {
-        $expire = time()+60*60*24*7;
-        
-        setcookie("username",$_POST['UserName'],$expire);
-        setcookie("userfullname",$user[1], $expire);
-        setcookie("userno",$user[2], $expire);
-        
-        $loggedIn=TRUE;
-        }
+      if ($_POST['UserName'] == "")
+        { $g->error="name_empty"; }
       else
-        { $g->error="name_wrong"; }
+        {
+        $user = GetUser($_POST['UserName']);
+        if ($user[1] != "")
+          {
+          $expire = time()+60*60*24*7;
+
+          setcookie("username",$_POST['UserName'],$expire);
+          setcookie("userfullname",$user[1], $expire);
+          setcookie("userno",$user[2], $expire);
+
+          $loggedIn=TRUE;
+          }
+        else
+          { $g->error="name_wrong"; }
+        }
       }
+    else
+      { $g->error="name_empty"; }
+    }
+
+  if ($loggedIn)
+    {
+    header( 'Location: matrix.php' );
     }
   else
-    { $g->error="name_empty"; }
-  }
-
-if ($loggedIn)
-  {
-  header( 'Location: matrix.php' );
-  }
-else
-  {
-  $tmpl->assign('g',$g);
-  $tmpl->display('login.tpl');
+    {
+    $tmpl->assign('g',$g);
+    $tmpl->display('login.tpl');
+    }
   }
 ?>
