c82524de6ec176ac4a5b18388193b2262dc4f3b9
thequantumtiger <thequantumtiger@033006fc-0a1e-2fcd-f6aa-1b5260ef9ce4>
2011-10-13 11:27:07 +0000

    Change database name and clarify comments

--- environment.php
+++ environment.php
@@ -1,5 +1,12 @@
 <?php
-$env['database'] = "taskmon";
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Enviroment file holds the settings which enable PET to connect to the MySQL Database
+// Database name must match the MySQL database name. The default from in the database_setup.sql
+// script is PET, but if you change that you also need to change the databse name here
+// The host is 'localhost' if your MySQL server is on the same server as your Apache server
+// otherwise it needs to be the local IP address of the MySQL Server
+/////////////////////////////////////////////////////////////////////////////////////////////////
+$env['database'] = "PET";
 $env['host'] ='localhost';
 $env['dbUser'] = '***'; // Database Username here
 $env['pw'] = '***'; // Database password here
